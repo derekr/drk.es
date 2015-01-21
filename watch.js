ddd7b123586b2a083d38f60cc31e5611f85858d6
Derek <derekr@me.com>
2015-01-20 16:11:40 -0800

    Add article index
    - Adds media queries
    - Adds new watch script

--- watch.js
+++ watch.js
@@ -1,11 +1,23 @@
 var serve = require('metalsmith-serve');
-var watch = require('metalsmith-watch');
+var watch = require('metalsmith-simplewatch');
 
-require('./lib/metalsmith')()
-    .use(watch({
-        livereload: true
-    }))
-    .use(serve())
-    .build(function (err) {
-        if (err) return console.error(err);
-    });
+// require('./lib/metalsmith')()
+//     .use(watch({
+//
+//         livereload: true
+//     }))
+//     .use(serve())
+//     .build(function (err) {
+//         if (err) return console.error(err);
+//     });
+
+watch({
+    buildFn: function () {
+        require('./lib/metalsmith')().build(function (err) {
+            if (err) return console.error(err);
+        });
+    },
+
+    buildPath: __dirname + '/build',
+    srcPath: __dirname + '/src'
+});
